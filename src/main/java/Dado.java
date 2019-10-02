16c2ca4ada22040cb6be5975e42823c2679b4726
dam2a <amateu@elpuig.xeill.net>
2019-10-02 19:54:33 +0200

    Initial commit

---
+++ src/main/java/Dado.java
@@ -0,0 +1,20 @@
+import java.lang.reflect.Array;
+
+public class Dado {
+    int dado, dado2, dado3;
+    int max = 6, min = 1;
+    int range = max - min + 1;
+
+    public int tirarDados() {
+        dado = (int) (Math.random() * range) + min;
+        return dado;
+    }
+
+    public void imprimirDados(int n) {
+            System.out.println("DADO " + n);
+        }
+
+    public int getDado() {
+        return dado;
+    }
+}
---
+++ src/main/java/JugarDados.java
@@ -0,0 +1,25 @@
+
+
+public class JugarDados {
+    Dado dadoObj = new Dado();
+    int dado;
+    int victorias = 0;
+
+    void jugar () {
+        int dadoActual;
+        int victoria=0;
+
+        for (int i = 0; i < 3; i++) {
+            dadoObj.tirarDados();
+            dadoActual = dadoObj.getDado();
+            dadoObj.imprimirDados(dadoActual);
+            if (dado == dadoActual){
+                victoria++;
+            }
+        }
+        if (victoria==1){
+            System.out.println("Victoria");
+            victorias++;
+        }
+    }
+}
