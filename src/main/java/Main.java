06c5433ac42bbaac47589495704cc7f696615854
Angel Mateu Paris <44613791+xSoraSS@users.noreply.github.com>
2019-10-02 22:21:29 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
 
-    public class Main {
+public class Main {
 
-        public static void main(String[] args) {
 
-        JugarDados jugar = new JugarDados();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("¿Quieres jugar unas partidas de dados?     1.Si       2.No");
+        int opcion = sc.nextInt();
 
-        jugar.jugar();
+        if (opcion == 1) {
+            JugarDados jugar = new JugarDados();
+            jugar.jugar();
         }
     }
+}
