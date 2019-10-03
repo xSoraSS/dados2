f88352bcf8d01417653506db13e56688408673bb
Angel Mateu Paris <44613791+xSoraSS@users.noreply.github.com>
2019-10-03 15:38:33 +0200

    Update JugarDados.java

--- src/main/java/JugarDados.java
+++ src/main/java/JugarDados.java
@@ -1,39 +1,23 @@
-package com.company;
-
-import java.util.Scanner;
-
 public class JugarDados {
-    Scanner sc = new Scanner(System.in);
     Dado dadoObj = new Dado();
-    int dado = 0, victorias = 0, R = 1, partidas = 0;
-    void jugar () {
+    int dado;
+    int victorias = 0;
 
-        while (R==1) {
+    void jugar () {
         int dadoActual;
         int victoria=0;
-            dado=0;
 
-            for (int i = 0; i < 3; i++) {
-                dadoObj.tirarDados();
-                dadoActual = dadoObj.getDado();
-                dadoObj.imprimirDados(dadoActual);
-                if (dado == dadoActual) {
-                    victoria++;
-                }
-                dado = dadoActual;
+        for (int i = 0; i < 3; i++) {
+            dadoObj.tirarDados();
+            dadoActual = dadoObj.getDado();
+            dadoObj.imprimirDados(dadoActual);
+            if (dado == dadoActual){
+                victoria++;
             }
-            if (victoria == 2) {
-                System.out.println("Victoria");
-                victorias++;
-            }
-            partidas++;
-            System.out.println("Quieres volver a tirar?      1.Si             2.No");
-            R = sc.nextInt();
-            if (R==1){
-                victoria=0;
-            }else{
-                System.out.println("Partidas Jugadas: " + partidas + "\nVictorias: " + victorias);
-            }
+        }
+        if (victoria==1){
+            System.out.println("Victoria");
+            victorias++;
         }
     }
 }
